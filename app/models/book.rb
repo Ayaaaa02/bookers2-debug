5e2c75f105b63987c5cd170c50d122b93076c853
Ayaaaa02 <139942884+Ayaaaa02@users.noreply.github.com>
2023-09-06 12:21:40 +0000

    課題5

--- app/models/book.rb
+++ app/models/book.rb
@@ -10,6 +10,18 @@ class Book < ApplicationRecord
    favorites.exists?(user_id: user.id)
   end
   
+  def self.search_for(content, method)
+    if method == 'perfect'
+      Book.where(title: content)
+    elsif method == 'forward'
+      Book.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      Book.where('name LIKE ?', '%' + content)
+    else
+      Book.where('name LIKE ?', '%' + content + '%')
+    end
+  end
+  
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
